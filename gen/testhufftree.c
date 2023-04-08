c077c8f271495969b850caf5089052d9b0a785ca
chmieladr <01178824@pw.edu.pl>
2023-04-08 02:44:29 +0200

    Dodatkowe testy

---
+++ gen/testhufftree.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    int i, j;
+    FILE *out;
+
+    if(argc != 2) {
+        fprintf(stderr, "Too few/too many arguments! (argc == %d != 2)\n", argc);
+        return -1;
+    }
+	
+    out = fopen(argv[1], "wb");
+    if(out == NULL) {
+        fprintf(stderr, "File could not be opened!");
+        return -2;
+    }
+
+    for(i = 0; i < 256; i++)
+        for(j = 0; j < i; j++)
+            fprintf(out, "%c", (unsigned char)i);
+    fclose(out);
+	return 0;
+}
---
+++ gen/testmax.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    int i, j, k;
+    FILE *out;
+
+    if(argc != 2) {
+        fprintf(stderr, "Too few/too many arguments! (argc == %d != 2)\n", argc);
+        return -1;
+    }
+	
+    out = fopen(argv[1], "wb");
+    if(out == NULL) {
+        fprintf(stderr, "File could not be opened!");
+        return -2;
+    }
+    for(i = 0; i <= 12; i++)
+        for(j = 0; j < 256; j++)
+            for(k = 0; k < 256; k++)
+                fprintf(out, "%c%c", (unsigned char)j, (unsigned char)k);
+    fclose(out);
+	return 0;
+}
